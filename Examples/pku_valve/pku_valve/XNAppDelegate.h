81400841239e2bb4c989aa94e60b609f9dd7e0d2
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-12-17 22:36:08 +0800

    add iOS example

---
+++ Examples/pku_valve/pku_valve/XNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XNAppDelegate.h
+//  pku_valve
+//
+//  Created by wuhaotian on 12/17/13.
+//  Copyright (c) 2013 wuhaotian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
